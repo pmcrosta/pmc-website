d19c73ccc16808bf01dbc3f6b49ad82e9b15a40f
pmcrosta <pmcrosta@gmail.com>
2025-03-17 12:52:25 -0400

    index and about

--- gtm.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-FWYE2E48ZC"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-FWYE2E48ZC');
-</script>
